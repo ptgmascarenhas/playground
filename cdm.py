d010383bf0a33fffa9e046c6f882b94f34c7132f
Paulo Mascarenhas <ptgmascarenhas@gmail.com>
2021-11-06 14:42:23 +0100

    update cdm

--- cdm.py
+++ cdm.py
@@ -1,93 +1,99 @@
 def string_to_bin(string_s):
-    bin_list = ' '.join(format(ord(x), 'b') for x in string_s).split()
-    bin_list_list = []
-    for item in bin_list:
-        if len(item) == 7:
-            item = "0" + item
-        bin_list_list.append([int(x) for x in item])
-    return bin_list_list, len(string_s)
+    bin_str = ' '.join(format(ord(x), 'b') for x in string_s)
+    bin_list = [ int(x) for x in list(bin_str.replace(" ", ""))]
+    return bin_list
 
-def bin_to_string(bin_word):
+def bin_to_string(bin_list):
+    bin_word = ''.join(str(x) for x in bin_list)
     word = ""
-    for item in bin_word:
-        chars = [str(int(x)) for x in item]
-        chars = ' '.join(chars).replace(" ", "")
-        an_integer = int(chars, 2)
-        ascii_character = chr(an_integer)
-        word += ascii_character
+    bin_char = ""
+    for i in bin_word:
+        bin_char += str(i)
+        if len(bin_char) == 7:
+            word += chr(int(f"0b{bin_char}", 2))
+            bin_char = ""
     return word
 
-def multiply(msg, code):
+def mux(msg, code):
     sig = []
-    for item in msg:    
-        sig.append([ item[x]*code[x] for x in range(0, len(code)) ])
+    for x in msg:
+        for y in code:
+            sig.append(x*y)
     return sig
+
+def demux(sig, code):
+    opr1 = [ sig[x]*code[x%len(code)] for x in range(0, len(sig))]
+    opr2 = []
+    for i in range(0, len(opr1), len(code)):
+        num = 0
+        for j in range(0, len(code)):
+            num += opr1[i+j]
+        opr2.append(int(num/len(code)))
+    return opr2
 
 def sum(sig1, sig2):
-    sum_list = []
-    for index in range(0,len(sig1)):
-        sum_list.append([x + y for x, y in zip(sig1[index], sig2[index])])
-    return sum_list
+    return [ sig1[x]+sig2[x] for x in range(0, len(sig1)) ]
 
-def sum_dc(msg, dc):
-    sig = []
-    for item in msg:    
-        sig.append([ item[x]+dc[x] for x in range(0, len(dc)) ])
-    return sig
+def pnrz(bin_list):
+    return [ int((x-0.5)*2) for x in bin_list ]
+
+def pnrz_rev(bin_list):
+    return [ int(x/2+0.5) for x in bin_list ]
 
 ###############################################################################
 
 # Codes
-cod1 = [-1,  1, -1,  1, -1,  1, -1,  1]
-cod2 = [-1, -1, -1, -1,  1,  1,  1,  1]
-dc_p = [ 0.5,  0.5,  0.5,  0.5,  0.5,  0.5,  0.5,  0.5]
-dc_n = [-0.5, -0.5, -0.5, -0.5, -0.5, -0.5, -0.5, -0.5]
-
-# cod1 = [-2,  2, -2,  2, -2,  2, -2,  2]
-# cod2 = [-2, -2, -2, -2,  2,  2,  2,  2]
-# dc_p = [ 1,  1,  1,  1,  1,  1,  1,  1]
-# dc_n = [-1, -1, -1, -1, -1, -1, -1, -1]
+cod1 = [-1,  1]
+cod2 = [-1, -1]
 
 # Words
-word1 = "isso_eh_um_teste_____"
-word2 = "vamos_ver_se_funciona"
+word1 = "ene"
+word2 = "unb"
 
 # Get binary from word
-bin_word1, lng = string_to_bin(word1)
-bin_word2, lng = string_to_bin(word2)
+bin_word1 = string_to_bin(word1)
+bin_word2 = string_to_bin(word2)
+print(f"Binary word 1: {bin_word1}")
+print(f"Binary word 2: {bin_word2}\n")
 
-print(f"Binary word: {bin_word1}\n")
-
-# Add DC to remove the 0s
-bin_word1 = sum_dc(bin_word1, dc_n)
-bin_word2 = sum_dc(bin_word2, dc_n)
-
-print(f"Binary word (+DC): {bin_word1}\n")
+# Add NRZ to remove the 0s
+# msg1 = pnrz(bin_word1)
+# msg2 = pnrz(bin_word2)
+# print(f"Msg 1 (P-NRZ): {msg1}")
+# print(f"Msg 2 (P-NRZ): {msg2}\n")
+msg1 = (bin_word1)
+msg2 = (bin_word2)
 
 # Get product code*chars
-sig1 = multiply(bin_word1, cod1)
-sig2 = multiply(bin_word2, cod2)
-
-print(f"Multip: {sig1}\n")
+sig1 = mux(msg1, cod1)
+sig2 = mux(msg2, cod2)
+print(f"Mux msg 1: {sig1}")
+print(f"Mux msg 2: {sig2}\n")
 
 # Sum the signals
 sig = sum(sig1, sig2)
-
 print(f"Sum: {sig}\n")
 
 # Multiply again
-bin_rec1 = multiply(sig, cod1)
-bin_rec2 = multiply(sig, cod2)
-
-print(f"Mult Inv.: {sig}\n")
+bin_rec1 = demux(sig, cod1)
+bin_rec2 = demux(sig, cod2)
+print(f"Demux msg 1: {bin_rec1}")
+print(f"Demux msg 2: {bin_rec2}\n")
 
 # Remove DC to remove the 0s
-bin_word1 = sum_dc(bin_word1, dc_p)
-bin_word2 = sum_dc(bin_word2, dc_p)
+# bin_word1_rec = pnrz_rev(bin_rec1)
+# bin_word2_rec = pnrz_rev(bin_rec2)
+# print(f"Binary word 1: {bin_word1_rec}")
+# print(f"Binary word 2: {bin_word2_rec}\n")
+bin_word1_rec = (bin_rec1)
+bin_word2_rec = (bin_rec2)
 
-print(f"Binary word (-DC): {bin_word1}\n")
-
-result1 = bin_to_string(bin_word1)
-result2 = bin_to_string(bin_word2)
-
+result1 = bin_to_string(bin_word1_rec)
+result2 = bin_to_string(bin_word2_rec)
 print(f"Words: {result1} / {result2}\n")
+
+
+print(bin_word1)
+print(bin_word1_rec)
+print(bin_word2)
+print(bin_word2_rec)
